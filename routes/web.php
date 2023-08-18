6b711843c36eeadcfdf395523b7f82a16a173815
Mary16-G <maryc.pgonzalez03@gamil.com>
2023-08-18 14:58:46 -0600

    mi tercer commit

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return "vista de Bienvenida";
+});
+
+Route::get('/usuarios/{id}/{nombre?}', function($idusuario,$nombreusuario=null){
+    if($nombreusuario){
+        return "Hola Usuario" .$nombreusuario ."tu id es" .$idusuario;
+    }
+    else{
+        return "Hola tu id es:" .$idusuario;
+    }
 });
